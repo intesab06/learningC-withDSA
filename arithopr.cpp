3925bac6cc97e0fcae24ff3e4e1b41404caa8afa
Intesab Asif <151721447+intesab06@users.noreply.github.com>
2026-01-21 23:08:07 +0530

    Add files via upload

---
+++ arithopr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      int a = 8, b = 6;
+
+      cout << "sum = " << (a+b) << endl;
+      cout << "difference = " << (a-b) << endl;
+      cout << "multiply = " << (a*b) << endl;
+      cout << "divide = " << (a/b) << endl;
+      cout << "modulo = " << (a%b) << endl;
+    return 0;
+}
---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+   // integer data types
+    int age = 22 ;
+    cout << age << endl;
+
+   //  character data types
+    char name = 'I' ;
+    cout << name << endl;
+
+   // float data types
+    float percentage = 97.5f;
+    cout << percentage << endl;
+
+   // boolean data types
+    bool isSafe = true;
+   cout << isSafe << endl;
+   
+   // double data types
+    double price = 100.99 ;
+   cout << price << endl;
+
+    return 0;
+ }
