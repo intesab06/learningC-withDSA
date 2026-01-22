681fb046b49bae2f21d920350c40d357e3396a3f
Intesab Asif <151721447+intesab06@users.noreply.github.com>
2026-01-22 23:59:03 +0530

    Add files via upload

---
+++ newcode.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "intesab asif \n from RCET";
+    return 0;
+}
---
+++ relatopr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+     cout << (3 < 5) << endl;
+     cout << (3 > 5) << endl;
+     cout << (3 <= 5) << endl;
+     cout << (3 >= 5) << endl;
+     cout << (3 == 5) << endl;
+     cout << (3 != 5) << endl;
+     return 0;
+}
